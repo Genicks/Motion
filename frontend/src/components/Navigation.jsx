49172fe2a42b41c9e91243bcedef7b5f40b9f2bc
Genick's <genicks.html@gmail.com>
2023-12-22 21:03:20 -0400

    Today logic

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -18,22 +18,22 @@ function Nav(props) {
       <a className={currentRoute("/SUN")} id={(day === 0? "Today": null)} href="/SUN">
         SUN
       </a>
-      <a className={currentRoute("/MON")} href="/MON">
+      <a className={currentRoute("/MON")} id={(day === 1? "Today": null)} href="/MON">
         MON
       </a>
-      <a className={currentRoute("/TUE")} href="/TUE">
+      <a className={currentRoute("/TUE")} id={(day === 2? "Today": null)} href="/TUE">
         TUE
       </a>
-      <a className={currentRoute("/WED")} href="/WED">
+      <a className={currentRoute("/WED")} id={(day === 3? "Today": null)} href="/WED">
         WED
       </a>
-      <a className={currentRoute("/THU")} href="/THU">
+      <a className={currentRoute("/THU")} id={(day === 4? "Today": null)} href="/THU">
         THU
       </a>
-      <a className={currentRoute("/FRI")} href="/FRI">
+      <a className={currentRoute("/FRI")} id={(day === 5? "Today": null)} href="/FRI">
         FRI
       </a>
-      <a className={currentRoute("/SAT")} href="/SAT">
+      <a className={currentRoute("/SAT")} id={(day === 6? "Today": null)} href="/SAT">
         SAT
       </a>
     </div>
